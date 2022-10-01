09330fba6e5964c522aeda3eaa2afa01255ac6a9
Georgi-Progger <hugaeag@gmail.com>
2022-10-01 20:37:16 +0300

    Добавьте файлы проекта.

---
+++ ASPProd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPProd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
